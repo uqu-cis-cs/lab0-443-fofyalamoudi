ea985406702fc642931487d5c634c38ce4e731ca
fofyalamoudi <127768176+fofyalamoudi@users.noreply.github.com>
2023-03-19 08:38:26 +0000

    commiting first time

--- src/main/java/edu/uqu/cs/App.java
+++ src/main/java/edu/uqu/cs/App.java
@@ -1,4 +1,6 @@
 package edu.uqu.cs;
+
+import java.util.Scanner;
 /*
  * Lab 0 
  * CS 1312
@@ -8,51 +10,93 @@ package edu.uqu.cs;
 * Make sure to complete and submit your lab
 */
 
+public class App {
 
-public class App{
-
-/**  
-* Complete method "twisters" definition below. The method prints all numbers from 1 to 110, 11 numbers per line. 
-* Also, it does the following:
-* - If the number is a multiple of 2, print "Tweetle"
-* - If the number is a multiple of 4, print "Beetle"
-* - If the number is a multiple of 6, print "Poodle"
-* - Otherwise just print the number
-* 
-* Output: 
-*        1 Tweetle 3 TweetleBeetle 5 TweetlePoodle 7 TweetleBeetle 9 Tweetle 11
-*        TweetleBeetlePoodle 13 Tweetle 15 TweetleBeetle 17 TweetlePoodle 19 TweetleBeetle 
-*/
-
-/**
-* Write a method named "phoneKeypad" that takes a string parameter (str)
-* then it replaces each letter in (str) by a number that represnts it 
-* in a phone keypad as follows: 
-* ABC = 2;
-* DEF = 3;
-* GHI = 4;
-* JKL = 5;
-* MNO = 6;
-* PQRS = 7;
-* TUV = 8; 
-* WXYZ = 9
-*
-* Example:
-* Input: calling batman
-* Output: 2255464228626
-*
-*/
- 
-    public static void main(String [] args) {
-        
-
-        /* Write your code here */
-     
-       //call method twisters()
-       //prompt user to enter a string 
-       //call method phoneKeypad(string)
-
-
+    public static void main(String[] args) {
+        Scanner fofy = new Scanner(System.in);
+        twisters();
+       
+        System.out.print("Enter");
+        phoneKeypad(fofy.nextLine());
     }
+    
+    public static void twisters(){
+        for (int i = 1; i <= 110; i++) {
+            if (i % 2 == 0 || i % 4 == 0 || i % 6 == 0) {
+                if (i % 2 == 0) {
+                    System.out.print("Tweetle");
 
+                }
+                if (i % 4 == 0) {
+                    System.out.print("Beetle");
+
+                }
+                if (i % 6 == 0) {
+                    System.out.print("Poodle");
+                }
+            } else {
+                System.out.print(" " + i + " ");
+            }
+            if (i%11 ==0) {
+                System.out.println();
+            }
+
+        }
+    }
+    public static void phoneKeypad(String str) {
+
+        
+        String out = "";
+        
+        // loop for each char in the text
+        for (char ch : str.toUpperCase().toCharArray()) {
+            // switch case for each 3 letters
+            switch (ch) {
+                case 'A':
+                case 'B':
+                case 'C':
+                    out += "2";
+                    break;
+                case 'D':
+                case 'E':
+                case 'F':
+                    out += "3";
+                    break;
+                case 'G':
+                case 'H':
+                case 'I':
+                    out += "4";
+                    break;
+                case 'J':
+                case 'K':
+                case 'L':
+                    out += "5";
+                    break;
+                case 'M':
+                case 'N':
+                case 'O':
+                    out += "6";
+                    break;
+                case 'P':
+                case 'Q':
+                case 'R':
+                case 'S':
+                   out += "7";
+                    break;
+                case 'T':
+                case 'U':
+                case 'V':
+                    out += "8";
+                    break;
+                case 'W':
+                case 'X':
+                case 'Y':
+                case 'Z':
+                    out += "9";
+                    break;
+            }
+        }
+        System.out.println(out);
+   
+    }
 }
